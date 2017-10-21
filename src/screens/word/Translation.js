096011edd8138170bee5a67fbfe93edc70737638
Gavin Gilmour <gavin.gilmour@gmail.com>
2017-10-21 18:00:20 +0100

    react 16 upgrade

--- src/screens/word/Translation.js
+++ src/screens/word/Translation.js
@@ -1,4 +1,5 @@
-import React, { PropTypes, Component } from "react";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 import { Ionicons } from "@expo/vector-icons";
 
 import { TouchableOpacity, View, Text, StyleSheet } from "react-native";
